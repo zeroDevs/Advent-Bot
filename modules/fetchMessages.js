0a1cae39e08e75bb05fd62455edb858f6f24a9bf
Matt Smith <mrmcchow@gmail.com>
2019-11-13 04:38:33 +0000

    Embed function

---
+++ modules/fetchMessages.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports = client => {
+  client.fetchMessages = channel => {
+    return channel
+      .fetchMessages({
+        limit: 100
+      })
+      .then(messages => {
+        return messages;
+      });
+  };
+};
