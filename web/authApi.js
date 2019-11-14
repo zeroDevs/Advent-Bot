33405af036d85696df0e1897d648b93fc3061b0b
Ankur Anant <ankuranant99@gmail.com>
2019-11-14 13:10:26 +0530

    change api method

--- web/authApi.js
+++ web/authApi.js
@@ -8,26 +8,26 @@ const tokens = require("../configs/tokens.json");
 
 //mongoose setup
 mongoose.connect(`${tokens.mongo}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 let solSchema = new mongoose.Schema({
-  url: String,
-  dayNumber: Number,
-  userName: String,
-  userid: Number,
-  language: String,
-  avatarUrl: String,
-  Time: { type: Date, default: Date.now }
+    url: String,
+    dayNumber: Number,
+    userName: String,
+    userid: Number,
+    language: String,
+    avatarUrl: String,
+    Time: { type: Date, default: Date.now }
 });
 
 let userSchema = new mongoose.Schema({
-  username: String, //format -> username#discrimanator
-  userid: Number,
-  badgePoint: Number,
-  avatarUrl: String,
-  point: Number
+    username: String, //format -> username#discrimanator
+    userid: Number,
+    badgePoint: Number,
+    avatarUrl: String,
+    point: Number
 });
 
 //snippet model
@@ -36,84 +36,82 @@ const Snippet = mongoose.model("Snippet", solSchema);
 let User = mongoose.model("User", userSchema);
 
 router.get("/login", (req, res) => {
-  res.redirect(
-    `https://discordapp.com/api/oauth2/authorize?client_id=${tokens.CLIENT_ID}&scope=identify%20guilds&response_type=code&redirect_uri=${tokens.redirect}`
-  );
+    res.redirect(
+        `https://discordapp.com/api/oauth2/authorize?client_id=${tokens.CLIENT_ID}&scope=identify%20guilds&response_type=code&redirect_uri=${tokens.redirect}`
+    );
 });
 
 router.get(
-  "/discord/callback",
-  catchAsync(async (req, res) => {
-    if (!req.query.code) throw new Error("NoCodeProvided");
-    const code = req.query.code;
-    const creds = btoa(`${tokens.CLIENT_ID}:${tokens.CLIENT_SECRET}`);
-    const response = await fetch(
-      `https://discordapp.com/api/oauth2/token?grant_type=authorization_code&scope=identify%20guilds&code=${code}&redirect_uri=${tokens.redirect}`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${creds}`,
-          "Content-Type": "application/json"
-        }
-      }
-    );
-    const json = await response.json();
-    console.log(json);
+    "/discord/callback",
+    catchAsync(async (req, res) => {
+        if (!req.query.code) throw new Error("NoCodeProvided");
+        const code = req.query.code;
+        const creds = btoa(`${tokens.CLIENT_ID}:${tokens.CLIENT_SECRET}`);
+        const response = await fetch(
+            `https://discordapp.com/api/oauth2/token?grant_type=authorization_code&scope=identify%20guilds&code=${code}&redirect_uri=${tokens.redirect}`,
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Basic ${creds}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        );
+        const json = await response.json();
+        console.log(json);
 
-    jwt.sign(
-      { aToken: json.access_token },
-      "y3l10whulk",
-      { expiresIn: "2d" },
-      (err, token) => {
-        if (err) throw err;
-        res.redirect(`${tokens.redirect_front}/?token=${token}`);
-      }
-    );
-  })
+        jwt.sign(
+            { aToken: json.access_token },
+            "y3l10whulk",
+            { expiresIn: "2d" },
+            (err, token) => {
+                if (err) throw err;
+                res.redirect(`${tokens.redirect_front}/?token=${token}`);
+            }
+        );
+    })
 );
 
 router.post("/submit", (req, res) => {
-  console.log(req.body);
-  const userData = req.body;
-  // res.status(200).json({error: null, isSuccessful: true});
+    console.log(req.body);
+    const userData = req.body;
+    // res.status(200).json({error: null, isSuccessful: true});
 
-  //check if user exist
-  User.findOne({ username: userData.name }, (err, userFound) => {
-    if (err) {
-      console.log(error);
-    }
-    if (userFound) {
-      //check if url already exist
-      Snippet.findOne({ url: userData.url }, (err, urlExist) => {
-        if (err) console.log(err);
-
-        console.log(urlExist);
-        if (urlExist) {
-          res
-            .status(400)
-            .json({
-              error: "URL already exist",
-              isSuccessful: false,
-              data: {}
-            });
-        } else {
-          //add more stuff
+    //check if user exist
+    User.findOne({ username: userData.name }, (err, userFound) => {
+        if (err) {
+            console.log(error);
         }
-      });
-    }
-  });
+        if (userFound) {
+            //check if url already exist
+            Snippet.findOne({ url: userData.url }, (err, urlExist) => {
+                if (err) console.log(err);
+
+                console.log(urlExist);
+                if (urlExist) {
+                    res.status(400).json({
+                        error: "URL already exist",
+                        isSuccessful: false,
+                        data: {}
+                    });
+                } else {
+                    //add more stuff
+                }
+            });
+        }
+    });
 });
 
 function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  if (typeof bearerHeader !== "undefined") {
-    const bearer = bearerHeader.replace(/^JWT\s/, ``).split(" ");
-    const bearerToken = bearer[1].replace(/^"(.*)"$/, "$1");
-    req.token = bearerToken;
-    next();
-  } else {
-    res.status(403).json({ error: "token error", isSuccessful: false });
-  }
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.replace(/^JWT\s/, ``).split(" ");
+        const bearerToken = bearer[1].replace(/^"(.*)"$/, "$1");
+        req.token = bearerToken;
+        next();
+    } else {
+        res.status(403).json({ error: "token error", isSuccessful: false });
+    }
 }
 
 module.exports = router;
