3aa234ea3cd03f0edc7b04f8439e2be2ab9f08e4
Matt Smith <mrmcchow@gmail.com>
2019-12-03 02:57:36 +0000

    updated

--- web/routes/authApi.route.js
+++ web/routes/authApi.route.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const btoa = require("btoa");
 const fetch = require("node-fetch");
-const requestN = require('request');
+const requestN = require("request");
 
 const { catchAsync } = require("../utils");
 const { estDay, estTime } = require("../utils/date.utils");
@@ -133,6 +133,10 @@ router.post("/submit", verifyToken, (req, res) => {
     });
 
     if (!isTokenValid) {
+        errorWebhook({
+            errorTitle: "INVALID TOKEN",
+            errorDesc: `req.token ${req.token}, tokens.jwtToken ${tokens.jwtToken}`
+        });
         console.log("INVALID TOKEN");
         return res.status(400).json({
             error: "Invalid Token",
@@ -143,33 +147,31 @@ router.post("/submit", verifyToken, (req, res) => {
 
     //extract date from request
     // convert epoch to human-readable form
-    let submittedDate = new Date(userData.date);
-    console.log(typeof submittedDate);
+    let submittedDate = userData.date;
 
-    if (
-        submittedDate.getDate() > estDay() ||
-        submittedDate.getMonth() + 1 != 12 ||
-        submittedDate.getFullYear() != 2019
-    ) {
-        console.log("INVALID DATE", submittedDate, submittedDate.getDate(), estDay());
+    if (submittedDate > estDay()) {
+        console.log("INVALID DATE", submittedDate, submittedDate, estDay());
 
         //send webhook if failed
         requestN.post({
-          headers: {'content-type': 'application/json'},
-          url: tokens.log_webhook,
-          body: {
-              "username": "Logger",
-              "avatar_url": "https://purepng.com/public/uploads/large/purepng.com-robotrobotprogrammableautomatonelectronicscyborg-1701528371687rcmuo.png",
-              "content": "**Date Error**",
-              "embeds": [
-                {
-                  "title": `Username: ${userData.userName} \n UserId: ${userData.userId}`,
-                  "color": "14177041",
-                  "description": `UrlSubmitted: ${userData.url} \n Est: ${estDay()} :: SubDay: ${submittedDate.getDate()}`
-                }
-              ]
+            headers: { "content-type": "application/json" },
+            url: tokens.log_webhook,
+            body: {
+                username: "Logger",
+                avatar_url:
+                    "https://purepng.com/public/uploads/large/purepng.com-robotrobotprogrammableautomatonelectronicscyborg-1701528371687rcmuo.png",
+                content: "**Date Error**",
+                embeds: [
+                    {
+                        title: `Username: ${userData.userName} \n UserId: ${userData.userId}`,
+                        color: "14177041",
+                        description: `UrlSubmitted: ${
+                            userData.url
+                        } \n Est: ${estDay()} :: SubDay: ${submittedDate}`
+                    }
+                ]
             },
-          json: true
+            json: true
         });
 
         res.status(400).json({
@@ -185,6 +187,10 @@ router.post("/submit", verifyToken, (req, res) => {
     //check if user exist
     User.findOne({ userid: userData.userId.toString() }, (err, userFound) => {
         if (err) {
+            errorWebhook({
+                errorTitle: "check if user exist",
+                errorDesc: err
+            });
             console.error("FIND USER ERROR:", error);
         }
         if (!userFound) {
@@ -202,7 +208,12 @@ router.post("/submit", verifyToken, (req, res) => {
         }
         //check if url already exist
         Solution.findOne({ url: userData.url }, (err, urlExist) => {
-            if (err) console.error(err);
+            if (err) {
+                errorWebhook({
+                    errorTitle: "check if url already exist",
+                    errorDesc: err
+                });
+            }
 
             if (urlExist) {
                 res.status(400).json({
@@ -214,7 +225,7 @@ router.post("/submit", verifyToken, (req, res) => {
             } else {
                 Solution.find(
                     {
-                        dayNumber: submittedDate.getDate(),
+                        dayNumber: submittedDate,
                         userid: userData.userId.toString()
                     },
                     (err, sol) => {
@@ -225,15 +236,15 @@ router.post("/submit", verifyToken, (req, res) => {
                                 console.log(sol.length);
                                 for (let i = 0; i < sol.length; i++) {
                                     if (userData.langName === sol[i].langName) {
+                                        errorWebhook({
+                                            errorTitle: "URL EXISTS",
+                                            errorDesc: `${userData.userName} tried to submit a solution for day ${submittedDate} in ${userData.langName} that has already been submitted.`
+                                        });
                                         console.error(
-                                            `Solution for day ${submittedDate.getDate()} in ${
-                                                userData.langName
-                                            } is already submitted.`
+                                            `Solution for day ${submittedDate} in ${userData.langName} is already submitted.`
                                         );
                                         res.status(400).json({
-                                            error: `Solution for day ${submittedDate.getDate()} in ${
-                                                userData.langName
-                                            } is already submitted.`,
+                                            error: `Solution for day ${submittedDate} in ${userData.langName} is already submitted.`,
                                             isSuccessful: false,
                                             data: {}
                                         });
@@ -245,10 +256,10 @@ router.post("/submit", verifyToken, (req, res) => {
                                 localBadgePoint = 1;
                             } else {
                                 //user hasn't submitted this day's solution
-                                if (submittedDate.getDate() == estDay()) {
+                                if (submittedDate == estDay()) {
                                     //today's solution --> point+2
                                     localPoint = 2;
-                                } else if (submittedDate.getDate() < estDay()) {
+                                } else if (submittedDate < estDay()) {
                                     //previous day's solution
                                     localPoint = 1;
                                 }
@@ -259,7 +270,7 @@ router.post("/submit", verifyToken, (req, res) => {
                         Solution.create(
                             {
                                 url: userData.url,
-                                dayNumber: submittedDate.getDate(),
+                                dayNumber: submittedDate,
                                 userName: userData.userName,
                                 userid: userData.userId.toString(),
                                 avatarUrl: userData.avatarUrl,
@@ -267,7 +278,12 @@ router.post("/submit", verifyToken, (req, res) => {
                                 Time: estTime()
                             },
                             async (err, done) => {
-                                if (err) console.error(err);
+                                if (err) {
+                                    errorWebhook({
+                                        errorTitle: "Adding Solution To Database",
+                                        errorDesc: err
+                                    });
+                                }
                                 if (done) {
                                     const stats = await StatsService.getStats();
                                     const recent = await SolutionsService.getRecentSolutions(6);
@@ -283,19 +299,19 @@ router.post("/submit", verifyToken, (req, res) => {
                                         ],
                                         recent: rSols
                                     });
+
+                                    // Create a embed in #submissions
+                                    submissionWebhook({
+                                        username: userData.userName,
+                                        url: userData.url,
+                                        day: submittedDate,
+                                        thumb: userData.avatarUrl,
+                                        lang: userData.langName,
+                                        time: estTime()
+                                    });
                                 }
                             }
                         );
-
-                        // Create a embed in #submissions
-                        submissionWebhook({
-                            username: userData.userName,
-                            url: userData.url,
-                            day: submittedDate.getDate(),
-                            thumb: userData.avatarUrl,
-                            lang: userData.langName,
-                            time: estTime()
-                        });
 
                         //update user points
                         User.findOneAndUpdate(
@@ -308,7 +324,12 @@ router.post("/submit", verifyToken, (req, res) => {
                             },
                             { upsert: true },
                             (err, doc) => {
-                                if (err) console.error(err);
+                                if (err) {
+                                    errorWebhook({
+                                        errorTitle: "Updating User Points",
+                                        errorDesc: err
+                                    });
+                                }
                                 return res.status(200).json({
                                     error: null,
                                     isSuccessful: true,
@@ -319,7 +340,12 @@ router.post("/submit", verifyToken, (req, res) => {
 
                         //add language used to array in user model
                         User.findOne({ userid: userData.userId.toString() }, (err, user) => {
-                            if (err) console.error(err);
+                            if (err) {
+                                errorWebhook({
+                                    errorTitle: "add language used to array in user model",
+                                    errorDesc: err
+                                });
+                            }
                             if (user) {
                                 console.log("FINDONE USER:", user);
                                 if (user.langArray.includes(userData.langName.toLowerCase())) {
