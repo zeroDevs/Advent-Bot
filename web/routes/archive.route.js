a383d2dec5c299e837b8bb201aed0d21ac6caa83
Ankur Anant <ankuranant99@gmail.com>
2020-11-30 01:47:04 +0530

    updating route

--- web/routes/archive.route.js
+++ web/routes/archive.route.js
@@ -4,7 +4,7 @@ route.get("/:year", (req, res) => {
   console.log(req.params.year)
   try {
     const json = require(`../archive_data/${req.params.year}.json`);
-    res.status(200).json(json);
+    res.status(200).json({solutions: json});
   } catch(ex) {
     console.log("File not found");
     res.status(400).json({error: "File not in archive."});
